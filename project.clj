093dff758c13666175814ee83b77647928ee3582
James Conroy-Finn <james@logi.cl>
2016-05-04 15:44:28 +0100

    Remove crypto-random dependency

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "https://github.com/jcf/oauth-one"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[crypto-random "1.2.0"]
-                 [org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [pandect "0.5.4"]
                  [prismatic/schema "1.1.0"]
                  [ring/ring-codec "1.0.0"]]
